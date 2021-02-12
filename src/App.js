32dd28fd1b9736a4b0bbcec1cdfd90a7ad1fba2d
dominicbutler <dominic.butler@exonar.com>
2021-02-12 16:24:57 +0000

    axios install

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
-function App() {
-    return <div className='App'>app</div>;
-}
+import axios from 'axios';
 
-export default App;
+const getData = () => {
+    axios
+        .get(
+            'https://api.nasa.gov/neo/rest/v1/feed?start_date=2021-02-01&end_date=2021-02-07&api_key=aKGF3JhSHt1jCVbfXtajzH17NdWtIDg2m59HEOqn'
+        )
+        .then(function (response) {
+            // handle success
+            console.log(response);
+        })
+        .catch(function (error) {
+            // handle error
+            console.log(error);
+        });
+};
+
+export default function App() {
+    return (
+        <div className='App'>
+            <button onClick={getData}>get</button>
+        </div>
+    );
+}
