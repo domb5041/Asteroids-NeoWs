0ee0d521858d1a772bef60ae358f3edb97f6c662
dominicbutler <dominic.butler@exonar.com>
2021-02-12 16:54:31 +0000

    d3 intro

---
+++ src/TimeSeriesSparkLineScatterPlot.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default ({ data, height, selectX, selectY, width }) => (
+    <svg className='container' height={height} width={width}>
+        {/* Our visualization should go here. */}
+    </svg>
+);
